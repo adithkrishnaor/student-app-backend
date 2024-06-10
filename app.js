ebde387b8d1eb5db2f1b86c2e15a05b8f66357ac
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-10 16:24:53 +0530

    added strure

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express= require("express")
+const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const student = require("./models/student")
 
 const app = express()
 app.use(cors())
@@ -13,6 +14,9 @@ app.get("/contact",(req,res)=>{
     res.send("Welcome")
 })
 
+app.get("/add",(req,res)=>{
+    res.send("AddTest")
+})
 app.listen(8080,()=>{
     console.log("Server started")
 })
---
+++ models/student.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema(
+    {
+        "name":String,
+        "rollNo":String,
+        "admNo":String,
+        "college":String
+    }
+)
+
+let studentmodel = mongoose.Model("students",schema);
+module.exports = {studentmodel}
